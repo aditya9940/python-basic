5db3630e31acfa60f3e4abfaddf469716703a89d
Abhishek Singh <36686933+Hexawz@users.noreply.github.com>
2020-07-30 18:44:32 +0530

    Update gui1.py

--- gui1.py
+++ gui1.py
@@ -1,6 +1,6 @@
 from tkinter import *
 top = Tk()
-top.geometry("200x200")
+top.geometry("400x800")
 c = Canvas(top,bg = "red",height = "200",width = 200)
 arc = c.create_arc((5,10,150,200),start = 0,extent = 150, fill= "white")
 c.pack()
